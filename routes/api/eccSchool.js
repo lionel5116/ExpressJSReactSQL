8b62f11d340cd9ab79abe3adfb45f43ba035bebb
p00149021@houstonisd.org Mag17615@7 <lioneljones5116@gmail.com>
2023-04-09 16:55:58 -0500

    add mongo DB Code ..

--- routes/api/eccSchool.js
+++ routes/api/eccSchool.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const eccSchool = require('../../models/eccSchool');
+const eccSchool = require('../../models/eccschool');
 
 router.post('/createECCSchool',
         async (req,res) => {
