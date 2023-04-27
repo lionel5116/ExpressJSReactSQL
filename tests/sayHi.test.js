a80994ecf6b149b566af1ab55905cef93ecd69c4
Jones <lioneljones5116@gmail.com>
2023-04-27 10:42:54 -0500

    added some unit test logic * 4

--- tests/sayHi.test.js
+++ tests/sayHi.test.js
@@ -5,10 +5,22 @@ const testfunctions = require('../testindex');
 test('Returns a greeting as a string', function () {
 	// should return a string
 	expect(typeof testfunctions.sayHi()).toBe('string');
+});
 
-    // should include the provided name
-	expect(testfunctions.sayHi('Merlin Jones').includes('Merlin')).toBe(true);
-
+test('function should return a type of number', function () {
 	// should return an integer
 	expect(typeof testfunctions.addNumbers()).toBe('number');
 });
+
+
+test('Should include the provided name', function () {
+	 // should include the provided name
+	 expect(testfunctions.sayHi('Merlin Jones').includes('Merlin')).toBe(true);
+});
+
+
+test('Adds two numbers together to a result', function () {
+	expect(testfunctions.addNumbers(2,5)).toBe(7);
+});
+
+
