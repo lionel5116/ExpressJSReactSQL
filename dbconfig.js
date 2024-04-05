13c800d95a729cd93eb6bf39e2e809e4d7a2a09d
Jones <lioneljones5116@gmail.com>
2024-04-05 11:16:11 -0500

    Database SQL Change HISD

--- dbconfig.js
+++ dbconfig.js
@@ -1,9 +1,9 @@
 const config = {
-    user:'u_DAC',
-    password:'iQlK3cSx[9',
-    server:'SQLINT01',
+    user:'u_SP_DAC_Int',
+    password:'$Zintegration#',
+    server:'SQLNONPSLDB01',
     port: 1433,
-    database:'SP_DAC',
+    database:'SP_DAC_Int',
     options: {
       trustedConnection: true,
       encrypt: true,
